e992b7cdf4e93fe3ded04a7e6846de25d6b88c9b
mt5225 <mt5225@gmail.com>
2017-05-08 11:51:57 -0700

    updated

--- js/custom.js
+++ js/custom.js
@@ -8,40 +8,45 @@
  * File Description: Place here your custom scripts
  */
 
-(function($){
-	$(document).ready(function(){
+(function ($) {
+	$(document).ready(function () {
 
 		// Notify Plugin - The below code (until line 42) is used for demonstration purposes only
 		//-----------------------------------------------
-		if (($(".main-navigation.onclick").length>0) && !Modernizr.touch ){
+		if (($(".main-navigation.onclick").length > 0) && !Modernizr.touch) {
 			$.notify({
 				// options
 				message: 'The Dropdowns of the Main Menu, are now open with click on Parent Items. Click "Home" to checkout this behavior.'
-			},{
-				// settings
-				type: 'info',
-				delay: 10000,
-				offset : {
-					y: 150,
-					x: 20
-				}
-			});
+			}, {
+					// settings
+					type: 'info',
+					delay: 10000,
+					offset: {
+						y: 150,
+						x: 20
+					}
+				});
 		};
-		if (!($(".main-navigation.animated").length>0) && !Modernizr.touch && $(".main-navigation").length>0){
+		if (!($(".main-navigation.animated").length > 0) && !Modernizr.touch && $(".main-navigation").length > 0) {
 			$.notify({
 				// options
 				message: 'The animations of main menu are disabled.'
-			},{
-				// settings
-				type: 'info',
-				delay: 10000,
-				offset : {
-					y: 150,
-					x: 20
-				}
-			}); // End Notify Plugin - The above code (from line 14) is used for demonstration purposes only
+			}, {
+					// settings
+					type: 'info',
+					delay: 10000,
+					offset: {
+						y: 150,
+						x: 20
+					}
+				}); // End Notify Plugin - The above code (from line 14) is used for demonstration purposes only
 
 		};
+
+		$('#submitButton').click(function (e) {
+			e.preventDefault();
+			window.location.href = "/";
+		});
 	}); // End document ready
 
 })(this.jQuery);
