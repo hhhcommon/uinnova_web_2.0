67d916197fd278f8cc4ecfbf423131e4eda2fdbe
jerry <jerry@uinnova.com>
2017-11-20 16:14:03 -0800

    update

--- srv/app.py
+++ srv/app.py
@@ -4,8 +4,9 @@ from flask import request
 import logging
 from logging.handlers import RotatingFileHandler
 from flask_cors import CORS
+import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def index():
@@ -18,6 +19,27 @@ def get_form_details():
     app.logger.info(content)
     return jsonify(content), 200
 
+@app.route('/dummy_api', methods=['GET'])
+def dummy_api():
+    return jsonify(msg='okey'), 200
+
+@app.route('/doorstatus', methods=['GET'])
+def doorstatus():
+    msg = {}
+    try:
+        conn = sqlite3.connect('/home/ubuntu/entrypass_backend/entrypass.db')
+        cur = conn.cursor()
+        cur.execute("SELECT rowid,* FROM live ORDER BY ROWID DESC LIMIT 1")
+        data = cur.fetchone()
+        msg=dict(zip(['ROWID', 'ETYPE','TRDATE','TRTIME','TRCODE','TRDESC', 'TRID', 'DEVNAME'], [x for x in data]))
+        conn.commit()
+    except sqlite3.Error, e:
+        print "Error %s:" % e.args[0]
+    finally:
+        if conn:
+            conn.close()
+    return jsonify(msg), 200
+
 if __name__ == '__main__':
     LOG_FILENAME = 'form.log'
     formatter = logging.Formatter(
@@ -27,4 +49,4 @@ if __name__ == '__main__':
     handler.setFormatter(formatter)
     app.logger.addHandler(handler)
     CORS(app)
-    app.run(host='0.0.0.0', port=9009, debug=True)
+    app.run(host='0.0.0.0', port=9006, debug=True)
